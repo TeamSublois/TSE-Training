42c0103bcc9feab9bd0660fbfedf2bcc941c2b7a
Benson Budiman <blbudima@ucsd.edu>
2018-05-09 02:26:27 -0700

    sprint3 complete.

--- sprint-3/routes/index.js
+++ sprint-3/routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-var data = require('../chirps.json')
+var db
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://blbudima:blbudima@ds119350.mlab.com:19350/chirps', (err, database) => {
+
+})
+
+MongoClient.connect('mongodb://blbudima:blbudima@ds119350.mlab.com:19350/chirps', (err, client) => {
+	if (err) return console.log(err)
+	db = client.db('chirps')
+})
+
+//router.set('view engine', 'ejs')
 
 router.get('/', function(req, res, next) {
-	res.render('index', data);
+	var cursor = db.collection('chirps').find().toArray((err, result) => {
+		if (err) return console.log(err)
+		res.render('index.ejs', {chirps: result})
+	})
 });
 
 router.post('/new', (req, res) => {
@@ -12,7 +28,10 @@ router.post('/new', (req, res) => {
 	req.body.comments = 0;
 	req.body.retweets = 0;
 	req.body.likes = 0;
-	data.chirps.push(req.body);
+	db.collection('chirps').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+		res.redirect('/')
+	})	
 })
 
 module.exports = router;
